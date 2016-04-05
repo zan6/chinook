df2002824b137a44350f724d086c1dfe6c11202b
Zan Javornik <zj4421@student.uni-lj.si>
2016-04-05 08:39:33 +0000

    Prikaz podrobnosti seznama predvajanj

--- public/js/script.js
+++ public/js/script.js
@@ -31,5 +31,14 @@ $(document).ready(function() {
     });
   });
   
+  /* Prikaz podrobnosti seznama predvajanj */
+  $('#playlists div span').click(function(){
+    var details = $(this);
+    $.get('/playlist/' + details.parent().attr('playlist'),function(data){
+        details.html(' (' + data.tracks + ' track' + (data.tracks != 1? 's': '') + ' | ' +
+        Math.round(data.time / 60000) + ' min | <strong>$' + Math.round(data.price) + '</strong> total)');
+    });
+  });
+  
   
 });
