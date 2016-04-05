4f25c8786424997ec65fad60fe95793086e2b18c
Zan Javornik <zj4421@student.uni-lj.si>
2016-04-05 08:13:44 +0000

    Prikaz določenega števila zapisov na stran

--- app.js
+++ app.js
@@ -5,12 +5,13 @@ if (!process.env.PORT)
 var sqlite3 = require('sqlite3').verbose();
 var db = new sqlite3.Database('chinook.sl3');
 
+var stZapisov = 20;
 
 /* calls callback with specified page's artists and artist's details */
 var artists = function(page, artist, details, callback) {
   db.all("SELECT Artist.ArtistId, Name, StarsNo " +
     "FROM Artist, Stars WHERE Artist.ArtistId = Stars.ArtistId " +
-    "ORDER BY Name LIMIT 33 OFFSET ($page - 1) * 33",
+    "ORDER BY Name LIMIT "+ stZapisov +" OFFSET ($page - 1) * "+ stZapisov +"",
     {$page: page}, function(error, rows) {
       if (error) {
         console.log(error);
@@ -19,7 +20,7 @@ var artists = function(page, artist, details, callback) {
         var result = '<div id="artists">';
         for (var i = 0; i < rows.length; i++) {
           var selected = rows[i].ArtistId == artist;
-          result += '<div id="' + rows[i].ArtistId + '"><span class="numbers">' + (page * 33 + i - 32) + '.</span>' +
+          result += '<div id="' + rows[i].ArtistId + '"><span class="numbers">' + (page * stZapisov + i - (stZapisov-1)) + '.</span>' +
             '<a href="/artists/' + page + (!selected? '/details/' + rows[i].ArtistId: '') + '#' + rows[i].ArtistId + '">' +
             '<button type="button" class="btn btn-default' + (selected? ' selected': '') + '">' +
             rows[i].Name + '</button></a><span class="stars">';
@@ -190,14 +191,14 @@ app.get('/pages', function(request, response) {
       console.log(error);
       response.sendStatus(500);
     } else
-      response.send({pages: Math.ceil(row.Artists / 33)});
+      response.send({pages: Math.ceil(row.Artists / stZapisov)});
   });
 });
 // 1. zagon streznika:
 app.listen(process.env.PORT, function() {
   console.log('Strežnik je pognan!');
 });
-//1. posredovanje vsebin,privzeta strab je artists/1
+//1. posredovanje vsebin,privzeta stran je artists/1
 app.get('/', function(request, response) {
   response.redirect('/artists/1');
 });
